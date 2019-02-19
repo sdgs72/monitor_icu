86f8e4178c649c0fa64369be52d8f9ed6ca9a9a3
Ke <gatechke@gmail.com>
2019-02-19 17:19:07 -0500

    new experiments

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,7 +3,7 @@
 set -x
 
 TARGET="death"
-EXPERIMENT="exp_2_d"
+EXPERIMENT="exp_5_c"
 
 DATA_DIR="./data"
 LOG_DIR="./logs"
@@ -14,20 +14,20 @@ python code/main.py \
   --phase="pipeline" \
   --model_type="rnn" \
   --rnn_type="lstm" \
-  --rnn_bidirectional \
-  --use_attention \
+  --nornn_bidirectional \
+  --nouse_attention \
   --batch_size=128 \
   --input_size=256 \
   --rnn_hidden_size=256 \
-  --num_epochs=20 \
+  --num_epochs=50 \
   --learning_rate=1e-3 \
   --train_data_split="train" \
   --eval_data_split="val" \
   --data_dir="${DATA_DIR}" \
   --target_label="${TARGET}" \
-  --block_size=6 \
-  --history_window=56 \
-  --prediction_window=4 \
+  --block_size=3 \
+  --history_window=112 \
+  --prediction_window=8 \
   --train_dataset_size=0 \
   --eval_dataset_size=0 \
   --rnn_layers=1 \
