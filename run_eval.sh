5728931ab72760d35a6cd0678d0baee2580f7b2c
Tianyi Cui <891632480@qq.com>
2019-02-20 18:43:47 -0500

    gitignore

--- run_eval.sh
+++ run_eval.sh
@@ -12,11 +12,7 @@ mkdir -p ${LOG_DIR}
 python code/main.py \
   --phase=inference \
   --batch_size=128 \
-<<<<<<< HEAD
-  --eval_data_split=val \
-=======
   --eval_data_split="val" \
->>>>>>> 86f8e4178c649c0fa64369be52d8f9ed6ca9a9a3
   --eval_dataset_size=0 \
   --checkpoint_dir="./experiments" \
   --experiment_name="${EXPERIMENT}" >> ${LOG_DIR}/${EXPERIMENT}_val.log 2>&1 &
@@ -24,11 +20,7 @@ python code/main.py \
 python code/main.py \
   --phase=inference \
   --batch_size=128 \
-<<<<<<< HEAD
-  --eval_data_split=test \
-=======
   --eval_data_split="test" \
->>>>>>> 86f8e4178c649c0fa64369be52d8f9ed6ca9a9a3
   --eval_dataset_size=0 \
   --checkpoint_dir="./experiments" \
   --experiment_name="${EXPERIMENT}" >> ${LOG_DIR}/${EXPERIMENT}_test.log 2>&1 &
